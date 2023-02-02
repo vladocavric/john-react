65338b0978ed965acbf52f0a461920a44ec758d4
Vlado Cavric <vladocavric@gmail.com>
2023-02-02 23:28:51 +0100

    my slider

--- react-projects/07-slider/setup/src/App.js
+++ react-projects/07-slider/setup/src/App.js
@@ -1,9 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+	const [reviews] = useState(data);
+	const [selectedIdex, setSelectedIdex] = useState(0);
+	const { image, name, title, quote } = reviews[selectedIdex];
+	const handlePrev = () => {
+		setSelectedIdex((prevState) => {
+			if (prevState === 0) {
+				return reviews.length - 1;
+			}
+			return prevState - 1;
+		});
+	};
+	const handleNext = useCallback(() => {
+		setSelectedIdex((prevState) => {
+			if (prevState === reviews.length - 1) {
+				return 0;
+			}
+			return prevState + 1;
+		});
+	}, [reviews.length]);
+
+	useEffect(() => {
+		const time = setTimeout(() => {
+			handleNext();
+		}, 5000);
+		return () => {
+			clearTimeout(time);
+		};
+	}, [selectedIdex, handleNext]);
+	return (
+		<section className='section'>
+			<div className='title'>
+				<h2>
+					<span>/</span>Reviews
+				</h2>
+			</div>
+			<div className='section-center'>
+				<FiChevronLeft
+					className='prev'
+					onClick={handlePrev}
+					style={{ zIndex: 100 }}
+				/>
+				<article>
+					<img src={image} alt={name} className='person-img' />
+					<h4>{name}</h4>
+					<div className='title'>{title}</div>
+					<div className='text'>{quote}</div>
+					<FaQuoteRight className='icon' />
+				</article>
+				<FiChevronRight className='next' onClick={handleNext} />
+			</div>
+		</section>
+	);
 }
 
 export default App;
