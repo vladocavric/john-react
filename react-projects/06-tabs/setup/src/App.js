3726539c202ce325318cc939762d14dfdc27d894
Vlado Cavric <vladocavric@gmail.com>
2023-02-02 21:48:35 +0100

    finished tabs

--- react-projects/06-tabs/setup/src/App.js
+++ react-projects/06-tabs/setup/src/App.js
@@ -1,10 +1,58 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import { useState, useEffect } from 'react';
+import Job from './componets/Job';
+//
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = 'https://course-api.com/react-tabs-project';
 function App() {
-  return <h2>tabs project setup</h2>
+  const [isLoading, setIsLoading] = useState(true);
+	const [jobs, setJobs] = useState([]);
+	const [selectedIdex, setSelectedIndex] = useState(0);
+  const selectedJob = jobs.length > 0 && jobs[selectedIdex]
+
+  const handleSelect = (index) => {
+    setSelectedIndex(index)
+  }
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(url);
+				const data = await response.json();
+				setJobs(data);
+        setIsLoading(false)
+			} catch (err) {
+        setIsLoading(false)
+				console.log(err);
+			}
+		};
+
+		fetchData();
+		return () => {};
+	}, []);
+	return (
+    <>
+    {isLoading && <h2 className="loading">Loading...</h2>}
+		{jobs.length > 0 && <section className='section'>
+			<div className='title'>
+				<h2>experience</h2>
+				<div className='underline'></div>
+			</div>
+			<div className='jobs-container'>
+				<div className='btn-container'>
+					{jobs.map((job, index) => (
+						<button className={`job-btn ${job.id === selectedJob.id ? 'active-btn' : ''}`} key={job.id} onClick={() => handleSelect(index)}>
+							{job.company}
+						</button>
+					))}
+				</div>
+				<Job {...selectedJob} />
+			</div>
+			<button type='button' className='btn'>
+				more info
+			</button>
+		</section>}
+    </>
+	);
 }
 
-export default App
+export default App;
---
+++ react-projects/06-tabs/setup/src/componets/Job.jsx
@@ -0,0 +1,23 @@
+import { FaAngleDoubleRight } from 'react-icons/fa';
+
+const Job = ({title, company, dates, duties}) => {
+	return (
+		<>
+			<article className='job-info'>
+				<h3>{title}</h3>
+				<h4>{company}</h4>
+				<p className='job-date'>{dates}</p>
+				{duties.map((duty, index) => {
+					return (
+						<div key={index} className='job-desc'>
+							<FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+							<p>{duty}</p>
+						</div>
+					);
+				})}
+			</article>
+		</>
+	);
+};
+
+export default Job;
