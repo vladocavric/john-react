8c6266b926b58f9508973fa83b7e00d2e93307dc
Vlado Cavric <vladocavric@gmail.com>
2023-02-03 18:07:40 +0100

    finished lorem ipsum

--- react-advanced-v18/src/tutorial/4-forms/setup/2-multiple-inputs.jsx
+++ react-advanced-v18/src/tutorial/4-forms/setup/2-multiple-inputs.jsx
@@ -7,20 +7,28 @@ import React, { useState } from 'react';
 // dynamic object keys
 
 const ControlledInputs = () => {
-  const [firstName, setFirstName] = useState('');
-  const [email, setEmail] = useState('');
+  // const [firstName, setFirstName] = useState('');
+  // const [email, setEmail] = useState('');
+  const [person, setPerson] = useState({firstName: '', email: '', age: ''});
   const [people, setPeople] = useState([]);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setPerson(prevState => {
+      return {...person, [name]: value}
+    })
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (firstName && email) {
-      const person = { id: new Date().getTime().toString(), firstName, email };
+    if (person.firstName && person.email && person.age) {
+      const {firstName, email, age} = person
+      const newPerson = {firstName, email, age, id: new Date().getTime().toString()}
       console.log(person);
       setPeople((people) => {
-        return [...people, person];
+        return [...people, newPerson];
       });
-      setFirstName('');
-      setEmail('');
+      setPerson({firstName: '', email: '', age: ''})
     } else {
       console.log('empty values');
     }
@@ -35,8 +43,8 @@ const ControlledInputs = () => {
               type='text'
               id='firstName'
               name='firstName'
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
+              value={person.firstName}
+              onChange={handleChange}
             />
           </div>
           <div className='form-control'>
@@ -45,17 +53,27 @@ const ControlledInputs = () => {
               type='email'
               id='email'
               name='email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={person.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='email'>Age : </label>
+            <input
+              type='number'
+              id='age'
+              name='age'
+              value={person.age}
+              onChange={handleChange}
             />
           </div>
           <button type='submit'>add person</button>
         </form>
         {people.map((person, index) => {
-          const { id, firstName, email } = person;
+          const { id, firstName, email, age } = person;
           return (
             <div className='item' key={id}>
-              <h4>{firstName}</h4>
+              <h4>{firstName} - {age}</h4>
               <p>{email}</p>
             </div>
           );
--- react-projects/08-lorem-ipsum/setup/src/App.js
+++ react-projects/08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,48 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+	const [paragraphs, setParagraphs] = useState('');
+  const [texts, setTexts] = useState([]);
+  const handleChange = (e) => {
+    setParagraphs(e.target.value)
+  }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+    const isNotANo = isNaN(paragraphs)
+    console.log(isNotANo)
+    const times = +paragraphs < 1 || isNotANo ? 1 : +paragraphs > 8 ? 8 : +paragraphs
+    console.log(times)
+    const newTexts = []
+    for (let index = 0; index < times; index++) {
+      const textIndex = Math.floor(Math.random() * data.length)
+      console.log(textIndex)
+      newTexts.push(data[textIndex])
+    }
+    setTexts(newTexts)
+    setParagraphs('')
+	};
+	return (
+		<main>
+			<section className='section section-center'>
+				<h3>tired of boring lorem ipsum</h3>
+				<form onSubmit={handleSubmit} className='lorem-form'>
+					<div className='controlled-input'>
+						<label>
+							paragraphs{' '}
+							<input
+								type='text'
+								onChange={handleChange}
+								value={paragraphs}
+							/>
+						</label>
+					</div>
+					<button className='btn'>generate</button>
+				</form>
+        {!!texts && texts.map((text, index) => <p key={index} className='result'>{text}</p>)}
+				{/* <p className='result'>{data[0]}</p> */}
+			</section>
+		</main>
+	);
 }
 
 export default App;
